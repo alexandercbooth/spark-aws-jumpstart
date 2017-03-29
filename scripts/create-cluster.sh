81e4966f36f74976ec768de137f48f5e2ba5c88b
alexandercbooth <alexander.c.booth@gmail.com>
2017-03-29 08:12:00 -0400

    chore: adding cluster script :tada:

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -1,4 +1,4 @@
 echo "Launching cluster"
-export clusterId=`aws emr create-cluster --name "Spark cluster part III: Spark strikes back" --release-label emr-5.4.0 --applications Name=Spark \
+export clusterId=`aws emr create-cluster --name "Spark Cluster of my Own" --release-label emr-5.4.0 --applications Name=Spark \
 	--ec2-attributes SubnetId=$subnetId,KeyName=stella-key --instance-type m3.xlarge --instance-count 2 --use-default-roles`
 echo "Check the status of cluster with aws emr describe-cluster --cluster-id $clusterId"
