4ec284297e4248237fabf2f1e6d720a75d348167
alexandercbooth <alexander.c.booth@gmail.com>
2017-04-12 11:16:59 -0400

    chore: lighter installs and use py27

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -1,5 +1,2 @@
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-conda config --set always_yes yes --set changeps1 no
-conda install python=3.5 jupyter notebook numpy pandas
+sudo yum install tmux
+sudo pip install jupyter notebook
