87d5e110d441844adc6e4843307da11d1a8fc35b
alexandercbooth <alexander.c.booth@gmail.com>
2017-04-19 14:51:05 -0400

    chore: updating

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -1,4 +1,4 @@
 echo "Launching cluster"
-export clusterId=`aws emr create-cluster --name "Spork" --release-label emr-5.4.0 --applications Name=Spark \
+export clusterId=`aws emr create-cluster --name "Spock" --release-label emr-5.4.0 --applications Name=Spark \
 	--ec2-attributes SubnetId=$subnetId,KeyName=$keyName --instance-type m3.xlarge --instance-count 2 --use-default-roles`
 echo -e "Check the status of cluster with \n\naws emr describe-cluster --cluster-id $clusterId"
